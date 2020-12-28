75daaf678360611c55d8f615eec71a25c0d63bac
Yuri Grinshteyn <ygrinshteyn@google.com>
2020-12-28 10:09:50 -0800

    requests and errors work in OT/go

--- opentelemetry/go/main.go
+++ opentelemetry/go/main.go
@@ -2,57 +2,75 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
-	"os"
+	"time"
 
-	"go.opentelemetry.io/otel/api/global"
-	// "go.opentelemetry.io/otel/api/key"
-	"github.com/gorilla/mux"
-	"go.opentelemetry.io/otel/api/metric"
-	"go.opentelemetry.io/otel/exporter/metric/prometheus"
+	mexporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/metric"
+	"go.opencensus.io/tag"
+
+	"go.opentelemetry.io/otel/label"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/sdk/metric/controller/push"
+	"go.opentelemetry.io/otel/sdk/resource"
+
+	"os"
 )
 
 var (
-	env = os.Getenv("ENV")
+	env       = os.Getenv("ENV")
+	projectID = os.Getenv("PROJECT_ID")
 )
 
 func main() {
 
-	pusher, mainHandler, err := prometheus.InstallNewPipeline(prometheus.Config{})
-	if err != nil {
-		log.Panicf("failed to initialize prometheus exporter %v", err)
+	// Initialization. In order to pass the credentials to the exporter,
+	// prepare credential file following the instruction described in this doc.
+	// https://pkg.go.dev/golang.org/x/oauth2/google?tab=doc#FindDefaultCredentials
+	opts := []mexporter.Option{
+		mexporter.WithProjectID(projectID),
 	}
-	if pusher == nil {
-		log.Printf("no pusher")
-	} else {
-		log.Printf("have pusher")
+
+	// NOTE: In current implementation of exporter, this resource is ignored
+	resOpt := push.WithResource(resource.NewWithAttributes(
+		label.String("instance_id", "abc123"),
+		label.String("application", "example-app"),
+	))
+	pusher, err := mexporter.InstallNewPipeline(opts, resOpt)
+	if err != nil {
+		log.Fatalf("Failed to establish pipeline: %v", err)
 	}
 	defer pusher.Stop()
 
-	meter := global.MeterProvider().Meter("custom.googleapis.com/opentelemetry-metrics-demo")
-	if meter != nil {
-		log.Printf("have meter")
-	}
+	meter := pusher.MeterProvider().Meter("cloudmonitoring/example")
+	// Register request counter
+	totalRequestsCounter := metric.Must(meter).NewInt64Counter("total_requests")
+	// Register error counter
+	errorsCounter := metric.Must(meter).NewInt64Counter("failed_requests")
 
-	gaugeMetric := meter.NewFloat64Gauge("gauge",
-		metric.WithDescription("A gauge set to 1.0"),
-	)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ctx, _ := tag.New(context.Background())
+		// requestReceived := time.Now()
+		// count the request
+		totalRequestsCounter.Add(ctx, 1)
 
-	ctx := context.Background()
-	meter.
-		meter.RecordBatch(
-		ctx,
-		gaugeMetric.Measurement(1.0),
-	)
-
-	r := mux.NewRouter()
-	r.HandleFunc("/", mainHandler)
-
-	if env == "LOCAL" {
-		http.ListenAndServe("localhost:8080", r)
-	} else {
-		http.ListenAndServe(":8080", r)
-	}
+		// fail 10% of the time
+		if rand.Intn(100) > 90 {
+			// count the failed request
+			errorsCounter.Add(ctx, 1)
+			fmt.Fprintf(w, "error!")
+			// record latency for failure
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		} else {
+			delay := time.Duration(rand.Intn(1000)) * time.Millisecond
+			time.Sleep(delay)
+			// record latency for success
+			fmt.Fprintf(w, "Responded after "+delay.String())
+		}
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
